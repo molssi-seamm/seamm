acd3bdb32e1b601c99c51c9e0634802fe2dcd472
Paul Saxe <paul@thesaxes.net>
2019-03-21 12:08:09 -0400

    Switching to new logging-like printing.

--- molssi_workflow/start_node.py
+++ molssi_workflow/start_node.py
@@ -2,10 +2,14 @@
 
 """The start node in a workflow"""
 
-import molssi_workflow
 import logging
+import molssi_workflow
+import molssi_util.printing as printing
+from molssi_util.printing import FormattedText as __  # nopep8
 
 logger = logging.getLogger(__name__)
+job = printing.getPrinter()
+printer = printing.getPrinter('start')
 
 
 class StartNode(molssi_workflow.Node):
@@ -32,14 +36,26 @@ class StartNode(molssi_workflow.Node):
 
         next_node = super().describe(indent, json_dict)
 
-        self.job_output('')
-
         return next_node
 
     def run(self):
         """'Run' the start node, i.e. do nothing but print
         """
 
-        next_node = super().run()
-        self.job_output('')
+        next_node = super().run(printer)
         return next_node
+
+    def setup_printing(self, aprinter):
+        """Establish the handlers for printing as controlled by
+        options. The start step never writes to disk, so don't
+        create that handler.
+        """
+
+        # First remove an existing handlers
+        self.close_printing(printer)
+
+        # A handler for stdout
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(printing.JOB)
+        console_handler.setFormatter(self.formatter)
+        printer.addHandler(console_handler)
