c76864d4988e01425e35ace07dfd7bef95c48c4d
seamm <seamm@users.noreply.github.com>
2023-08-30 17:34:49 +0000

    deploy: 3ea1b901ce223bac1c90d3682cf403cd823a0b70

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2023.7.10',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '2023.8.30',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
